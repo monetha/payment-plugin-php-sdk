fe34c4284dac284e89f537dbe972c0c7382a88c5
Alexander Hitrov <alexander.hitrov@monetha.io>
2019-03-21 11:45:56 +0200

    dumb class added.

---
+++ src/Monetha/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hitrov
+ * Date: 2019-03-21
+ * Time: 11:43
+ */
+
+namespace Monetha;
+
+class Test
+{
+    public static function run() {
+        var_dump($_REQUEST);exit;
+    }
+}
