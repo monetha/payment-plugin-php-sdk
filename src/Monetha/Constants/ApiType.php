07e510e19941a7a9439adbabbf900c72d2a1f569
Alexander Hitrov <alexander.hitrov@monetha.io>
2019-04-18 12:21:30 +0300

    Revert "Sandbox API url changed."
    This reverts commit 5d2e4e5e

--- src/Monetha/Constants/ApiType.php
+++ src/Monetha/Constants/ApiType.php
@@ -5,5 +5,5 @@ namespace Monetha\Constants;
 class ApiType
 {
     const PROD = 'https://api.monetha.io/mth-gateway/';
-    const TEST = 'https://api.sandbox.monetha.io/mth-gateway/';
+    const TEST = 'https://api-sandbox.monetha.io/mth-gateway/';
 }
